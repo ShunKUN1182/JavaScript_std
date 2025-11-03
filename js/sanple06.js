0242684dee4400f3e26f0eb2ef3141fe726d21db
ShunKUN1182 <2250378@ecc.ac.jp>
2025-11-03 14:38:13 +0900

    ファイル整理

---
+++ js/sanple06.js
@@ -0,0 +1,27 @@
+console.log(10 > 11);
+
+let num1 = 10;
+let num2 = 8;
+
+console.log(num1 > num2);
+
+num1 = 15;
+num2 = 15;
+
+console.log(num1 <= num2);
+
+let str1 = "ECC Computer Collage";
+
+console.log(num1 <= str1.length);
+
+console.log(num1 < str1);
+
+let flag = "true";
+
+console.log(true == flag);
+
+console.log(false == "false");
+
+console.log(false == 0);
+
+console.log(false === 0);
