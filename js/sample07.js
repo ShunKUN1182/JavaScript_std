342d1a067cd0768fdec03004cde22ef71fac143d
ShunKUN1182 <2250378@ecc.ac.jp>
2025-11-10 16:52:19 +0900

    サンプル完成

---
+++ js/sample07.js
@@ -0,0 +1,107 @@
+const pref01 = "北海道";
+const pref02 = "青森";
+const pref03 = "岩手";
+const pref04 = "宮城";
+const pref05 = "秋田";
+const pref06 = "山形";
+const pref07 = "福島";
+const pref08 = "茨城";
+const pref09 = "栃木";
+const pref10 = "群馬";
+const pref11 = "埼玉";
+const pref12 = "千葉";
+const pref13 = "東京";
+const pref14 = "神奈川";
+const pref15 = "山梨";
+const pref16 = "長野";
+const pref17 = "新潟";
+const pref18 = "富山";
+const pref19 = "石川";
+const pref20 = "福井";
+const pref21 = "岐阜";
+const pref22 = "静岡";
+const pref23 = "愛知";
+const pref24 = "三重";
+const pref25 = "滋賀";
+const pref26 = "京都";
+const pref27 = "大阪";
+const pref28 = "兵庫";
+const pref29 = "奈良";
+const pref30 = "和歌山";
+const pref31 = "鳥取";
+const pref32 = "島根";
+const pref33 = "岡山";
+const pref34 = "広島";
+const pref35 = "山口";
+const pref36 = "徳島";
+const pref37 = "香川";
+const pref38 = "愛媛";
+const pref39 = "高知";
+const pref40 = "福岡";
+const pref41 = "佐賀";
+const pref42 = "長崎";
+const pref43 = "熊本";
+const pref44 = "大分";
+const pref45 = "宮崎";
+const pref46 = "鹿児島";
+const pref47 = "沖縄";
+
+const prefs = [
+    "北海道","青森","岩手","宮城","秋田","山形","福島",
+    "茨城","栃木","群馬","埼玉","千葉","東京","神奈川","山梨","長野",
+    "新潟","富山","石川","福井","岐阜","静岡","愛知","三重","滋賀",
+    "京都","大阪","兵庫","奈良","和歌山","鳥取","島根","岡山","広島","山口",
+    "徳島","香川","愛媛","高知","福岡","佐賀","長崎","熊本","大分",
+    "宮崎","鹿児島","沖縄",
+];
+
+console.log(prefs);
+
+console.log(pref01);
+console.log(pref02);
+console.log(pref03);
+console.log(pref04);
+console.log(pref05);
+console.log(pref06);
+console.log(pref07);
+console.log(pref08);
+console.log(pref09);
+console.log(pref10);
+console.log(pref11);
+console.log(pref12);
+console.log(pref13);
+console.log(pref14);
+console.log(pref15);
+console.log(pref16);
+console.log(pref17);
+console.log(pref18);
+console.log(pref19);
+console.log(pref20);
+console.log(pref21);
+console.log(pref22);
+console.log(pref23);
+console.log(pref24);
+console.log(pref25);
+console.log(pref26);
+console.log(pref27);
+console.log(pref28);
+console.log(pref29);
+console.log(pref30);
+console.log(pref31);
+console.log(pref32);
+console.log(pref33);
+console.log(pref34);
+console.log(pref35);
+console.log(pref36);
+console.log(pref37);
+console.log(pref38);
+console.log(pref39);
+console.log(pref40);
+console.log(pref41);
+console.log(pref42);
+console.log(pref43);
+console.log(pref44);
+console.log(pref45);
+console.log(pref46);
+console.log(pref47);
+
